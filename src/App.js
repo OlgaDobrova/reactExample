910fbf69eff79aead8fd800fecc7247a315da8dd
olga dobrova <dobrova_o_g@mail.ru>
2024-04-05 14:40:09 +0300

    useReducer вместо useState

--- src/App.js
+++ src/App.js
@@ -1,64 +1,54 @@
-import { useState, useCallback, useEffect, useMemo } from "react";
+import { useState, useReducer } from "react";
 import { Container } from "react-bootstrap";
 import "./App.css";
 
-const countTotal = (num) => {
-  console.log("couting...");
-  return num + 10;
-};
+// ф-ция модификации state
+//аргументы - state (сост до модификации), action (название действия, кот будем совершать со state)
+function reducer(state, action) {
+  switch (action.type) {
+    case "toggle":
+      return { autoplay: !state.autoplay };
+    case "slow":
+      return { autoplay: 300 };
+    case "fast":
+      return { autoplay: 700 };
+    case "custom":
+      return { autoplay: action.payload };
+    default:
+      throw new Error();
+  }
+}
 
-const Slider = (props) => {
+function init(inital) {
+  //что то там делаем
+  return { autoplay: inital };
+}
+
+const Slider = ({ inital }) => {
   const [slide, setSlide] = useState(0);
-  const [autoplay, setAutoplay] = useState(false);
+  // const [autoplay, setAutoplay] = useState(false);
+  //useReducer() - state и dispatch - ф-ция, меняющая его (обяз. нач с dispatch)
+  //аргументы - ф-ция, нач.состояние, ленивое создание нач.состояния
+  //можно так:
+  //const [autoplay, dispatch] = useReducer(reducer, { autoplay: false });
+  //можно так:
+  const [autoplay, dispatch] = useReducer(reducer, inital, init);
 
-  //если в завис-тях пустой массив, то ф-ция запустится 1 раз - при загрузке страницы - важно для оптимизации проекта
-  //useCallback - закэшировали ф-цию!
-  const getSomeImages = useCallback(() => {
-    console.log("как будто получили от сервера ответ - массив картинок");
-    return [
-      "https://img.freepik.com/premium-photo/spring-flowers-on-paper_176873-6829.jpg?w=1380",
-      "https://img.freepik.com/free-photo/cute-white-snowdrop-flowers-in-a-snowy-ground-the-start-of-a-spring_181624-15708.jpg?w=1380&t=st=1711113516~exp=1711114116~hmac=a85f3ccdeb2423a44209998879a305e0cdea2648dcfb8c7881653a5c499b6e81",
-      "https://uprostim.com/wp-content/uploads/2021/04/image010-2.jpg",
-    ];
-  }, []);
-
-  const changeSlide = (i) => {
+  function changeSlide(i) {
     setSlide((slide) => slide + i);
-  };
-
-  const toggleAutoplay = () => {
-    setAutoplay((autoplay) => !autoplay);
-  };
-
-  //useMemo - запускается в рендеринге
-  //Значение не пересчитывается при рендере, оно запомнено (мемоизировано), если состояние (state) не входит в стек зависимостей
-  //без использования useMemo значение ф-ции будет пересчитано при каждом рендере
-  //useMemo - закэшировали значение ф-ции!
-  const total = useMemo(() => {
-    return countTotal(slide);
-  }, [slide]);
-
-  const style = useMemo(
-    () => ({
-      color: slide > 4 ? "red" : "green",
-    }),
-    [slide]
-  );
-
-  useEffect(() => {
-    console.log("перем style пересчиталась");
-  }, [style]);
+  }
 
   return (
     <Container>
       <div className="slider w-50 m-auto">
-        <Slide getSomeImages={getSomeImages} />
-
+        <img
+          className="d-block w-100"
+          src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+          alt="slide"
+        />
         <div className="text-center mt-5">
-          Active slide {slide} <br /> {autoplay ? "auto" : null}
-        </div>
-        <div style={style} className="text-center mt-5">
-          Total slides: {total}
+          Active slide {slide} <br />
+          {autoplay.autoplay ? "auto" : null}
         </div>
         <div className="buttons mt-3">
           <button
@@ -73,8 +63,31 @@ const Slider = (props) => {
           >
             +1
           </button>
-          <button className="btn btn-primary me-2" onClick={toggleAutoplay}>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => dispatch({ type: "toggle" })}
+          >
             toggle autoplay
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => dispatch({ type: "slow" })}
+          >
+            sliw autoplay
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => dispatch({ type: "fast" })}
+          >
+            fast autoplay
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={(e) =>
+              dispatch({ type: "custom", payload: +e.target.textContent })
+            }
+          >
+            3000
           </button>
         </div>
       </div>
@@ -82,31 +95,8 @@ const Slider = (props) => {
   );
 };
 
-const Slide = ({ getSomeImages }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    setImages(getSomeImages());
-  }, [getSomeImages]);
-
-  return (
-    <>
-      {images.map((url, i) => (
-        <img key={i} className="d-block w-100" src={url} alt="slide" />
-      ))}
-    </>
-  );
-};
-
 function App() {
-  const [slider, setSlider] = useState(true);
-
-  return (
-    <>
-      <button onClick={() => setSlider(!slider)}>Click</button>
-      {slider ? <Slider /> : null}
-    </>
-  );
+  return <Slider inital={false} />;
 }
 
 export default App;
