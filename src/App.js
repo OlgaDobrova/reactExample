acd8aeb740ce1826b07a25610e0486cc3d2e548a
olga dobrova <dobrova_o_g@mail.ru>
2024-04-09 16:38:29 +0300

    Применение Компонентов высшего порядка

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
-import { useState, useReducer } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import "./App.css";
 
-// ф-ция модификации state
-//аргументы - state (сост до модификации), action (название действия, кот будем совершать со state)
-function reducer(state, action) {
-  switch (action.type) {
-    case "toggle":
-      return { autoplay: !state.autoplay };
-    case "slow":
-      return { autoplay: 300 };
-    case "fast":
-      return { autoplay: 700 };
-    case "custom":
-      return { autoplay: action.payload };
-    default:
-      throw new Error();
-  }
-}
+// Компонент высшего порядка - HOC - начинается всегда с with
+const withSlider = (BaseComponent, getData) => {
+  return (props) => {
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
 
-function init(inital) {
-  //что то там делаем
-  return { autoplay: inital };
-}
+    useEffect(() => {
+      setSlide(getData());
+    }, []);
 
-const Slider = ({ inital }) => {
-  const [slide, setSlide] = useState(0);
-  // const [autoplay, setAutoplay] = useState(false);
-  //useReducer() - state и dispatch - ф-ция, меняющая его (обяз. нач с dispatch)
-  //аргументы - ф-ция, нач.состояние, ленивое создание нач.состояния
-  //можно так:
-  //const [autoplay, dispatch] = useReducer(reducer, { autoplay: false });
-  //можно так:
-  const [autoplay, dispatch] = useReducer(reducer, inital, init);
+    function changeSlide(i) {
+      setSlide((slide) => slide + i);
+    }
 
-  function changeSlide(i) {
-    setSlide((slide) => slide + i);
-  }
+    return (
+      <BaseComponent
+        {...props}
+        slide={slide}
+        autoplay={autoplay}
+        setAutoplay={setAutoplay}
+        changeSlide={changeSlide}
+      />
+    );
+  };
+};
 
+const getDataFormFirstFetch = () => {
+  return 10;
+};
+const getDataFormSecondFetch = () => {
+  return 20;
+};
+
+const SliderFirst = (props) => {
+  const { slide, changeSlide } = props;
+  return (
+    <Container>
+      <div className="slider w-50 m-auto">
+        <img
+          className="d-block w-100"
+          src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+          alt="slide"
+        />
+        <div className="text-center mt-5">Active slide {slide}</div>
+        <div className="buttons mt-3">
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(-1)}
+          >
+            -1
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(1)}
+          >
+            +1
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+const SliderSecond = (props) => {
+  const { slide, changeSlide, autoplay, setAutoplay } = props;
   return (
     <Container>
       <div className="slider w-50 m-auto">
@@ -48,7 +77,7 @@ const Slider = ({ inital }) => {
         />
         <div className="text-center mt-5">
           Active slide {slide} <br />
-          {autoplay.autoplay ? "auto" : null}
+          {autoplay ? "auto" : null}
         </div>
         <div className="buttons mt-3">
           <button
@@ -63,31 +92,12 @@ const Slider = ({ inital }) => {
           >
             +1
           </button>
+
           <button
             className="btn btn-primary me-2"
-            onClick={() => dispatch({ type: "toggle" })}
+            onClick={() => setAutoplay((autoplay) => !autoplay)}
           >
             toggle autoplay
-          </button>
-          <button
-            className="btn btn-primary me-2"
-            onClick={() => dispatch({ type: "slow" })}
-          >
-            sliw autoplay
-          </button>
-          <button
-            className="btn btn-primary me-2"
-            onClick={() => dispatch({ type: "fast" })}
-          >
-            fast autoplay
-          </button>
-          <button
-            className="btn btn-primary me-2"
-            onClick={(e) =>
-              dispatch({ type: "custom", payload: +e.target.textContent })
-            }
-          >
-            3000
           </button>
         </div>
       </div>
@@ -95,8 +105,33 @@ const Slider = ({ inital }) => {
   );
 };
 
+// Здесь HOC подключает разные компоненты по одной логике
+const SliderWithFirstFetch = withSlider(SliderFirst, getDataFormFirstFetch);
+const SliderWithSecondFetch = withSlider(SliderSecond, getDataFormSecondFetch);
+
+// Здесь HOC дополняет компонент Hello своей логикой, не меняя его самого (применяется, например, для подключения метрики)
+// Это сокращенный вид компонента высшего порядка - HOC
+const withLogger = (WrappedComponent) => (props) => {
+  useEffect(() => {
+    console.log("first render!");
+  }, []);
+  return <WrappedComponent {...props} />;
+};
+
+const Hello = () => {
+  return <h1>Hello</h1>;
+};
+
+const HelloWithLogger = withLogger(Hello);
+
 function App() {
-  return <Slider inital={false} />;
+  return (
+    <>
+      <HelloWithLogger />
+      <SliderWithFirstFetch />
+      <SliderWithSecondFetch />
+    </>
+  );
 }
 
 export default App;
